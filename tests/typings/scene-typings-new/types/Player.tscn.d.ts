6edef3329c03d31023685fe89969409f09907af7
nnn3d <nnn3d@mail.ru>
2026-04-01 02:22:27 +0300

    style(typings): prettify generated typings

--- tests/typings/scene-typings-new/types/Player.tscn.d.ts
+++ tests/typings/scene-typings-new/types/Player.tscn.d.ts
@@ -1,53 +1,65 @@
 // AUTO-GENERATED — do not edit manually.
 
+type _PlayerTscn_Sprite2D = {
+  [__node_type]: Sprite2D;
+  [__node_parent]: _PlayerTscn_Tree;
+  [__node_children]: [
+    _PlayerTscn_AnimationPlayer
+  ];
+  "AnimationPlayer": _PlayerTscn_AnimationPlayer,
+  "AnimationPlayer/Body": _PlayerTscn_Body,
+  "AnimationPlayer/Body/Armor": _PlayerTscn_BodyArmor,
+};
+
+type _PlayerTscn_AnimationPlayer = {
+  [__node_type]: _GDGetInterfaceNode<_GodotScripts, "res://PlayerChild.gd">;
+  [__node_parent]: _PlayerTscn_Sprite2D;
+  [__node_children]: [
+    _PlayerTscn_Body
+  ];
+  "Body": _PlayerTscn_Body,
+  "Body/Armor": _PlayerTscn_BodyArmor,
+};
+
+type _PlayerTscn_Body = {
+  [__node_type]: Sprite2D;
+  [__node_parent]: _PlayerTscn_AnimationPlayer;
+  [__node_children]: [
+    _PlayerTscn_BodyArmor
+  ];
+  "Armor": _PlayerTscn_BodyArmor,
+}
+
+type _PlayerTscn_BodyArmor = {
+  [__node_type]: Sprite2D;
+  [__node_parent]: _PlayerTscn_Body;
+  [__node_children]: [];
+}
+
+type _PlayerTscn_CollisionShape2D = {
+  [__node_type]: CollisionShape2D;
+  [__node_parent]: _PlayerTscn_Tree;
+  [__node_children]: []
+};
+
+type _PlayerTscn_HealthBar = {
+  [__node_type]: ProgressBar;
+  [__node_parent]: _PlayerTscn_Tree;
+  [__node_children]: []
+};
+
 type _PlayerTscn_Tree = {
   [__node_root]: true,
   [__node_type]: _GDGetInterfaceNode<_GodotScripts, "res://Player.gd">;
   [__node_parent]: _GDGetInterfaceParent<__PlayerTscn__Parents>
-  [__node_children]: [_PlayerTscn_Tree['Sprite2D'], _PlayerTscn_Tree['CollisionShape2D'], _PlayerTscn_Tree['HealthBar']];
-  Sprite2D: {
-    [__node_type]: Sprite2D;
-    [__node_parent]: _PlayerTscn_Tree;
-    [__node_children]: [
-      _PlayerTscn_Tree['Sprite2D/AnimationPlayer']
-    ];
-    "AnimationPlayer": _PlayerTscn_Tree['Sprite2D/AnimationPlayer'],
-    "AnimationPlayer/Body": _PlayerTscn_Tree['Sprite2D/AnimationPlayer/Body'],
-    "AnimationPlayer/Body/Armor": _PlayerTscn_Tree['Sprite2D/AnimationPlayer/Body/Armor'],
-  };
-  "Sprite2D/AnimationPlayer": {
-    [__node_type]: _GDGetInterfaceNode<_GodotScripts, "res://PlayerChild.gd">;
-    [__node_parent]: _PlayerTscn_Tree['Sprite2D'];
-    [__node_children]: [
-      _PlayerTscn_Tree['Sprite2D/AnimationPlayer/Body']
-    ];
-    "Body": _PlayerTscn_Tree['Sprite2D/AnimationPlayer/Body'],
-    "Body/Armor": _PlayerTscn_Tree['Sprite2D/AnimationPlayer/Body/Armor'],
-  };
-  "Sprite2D/AnimationPlayer/Body": {
-    [__node_type]: Sprite2D;
-    [__node_parent]: _PlayerTscn_Tree['Sprite2D/AnimationPlayer'];
-    [__node_children]: [
-      _PlayerTscn_Tree['Sprite2D/AnimationPlayer/Body/Armor']
-    ];
-    "Armor": _PlayerTscn_Tree['Sprite2D/AnimationPlayer/Body/Armor'],
-  }
-  "Sprite2D/AnimationPlayer/Body/Armor": {
-    [__node_type]: Sprite2D;
-    [__node_parent]: _PlayerTscn_Tree['Sprite2D/AnimationPlayer/Body'];
-    [__node_children]: [];
-  }
-  CollisionShape2D: {
-    [__node_type]: CollisionShape2D;
-    [__node_parent]: _PlayerTscn_Tree;
-    [__node_children]: []
-  };
-  HealthBar: {
-    [__node_type]: ProgressBar;
-    [__node_parent]: _PlayerTscn_Tree;
-    [__node_children]: []
-  };
-  '%HealthBar': _PlayerTscn_Tree['HealthBar']
+  [__node_children]: [_PlayerTscn_Sprite2D, _PlayerTscn_CollisionShape2D, _PlayerTscn_HealthBar];
+  Sprite2D: _PlayerTscn_Sprite2D;
+  "Sprite2D/AnimationPlayer": _PlayerTscn_AnimationPlayer;
+  "Sprite2D/AnimationPlayer/Body": _PlayerTscn_Body;
+  "Sprite2D/AnimationPlayer/Body/Armor": _PlayerTscn_BodyArmor;
+  CollisionShape2D: _PlayerTscn_CollisionShape2D;
+  HealthBar: _PlayerTscn_HealthBar;
+  '%HealthBar': _PlayerTscn_HealthBar;
 }
 
 declare global {
@@ -61,7 +73,7 @@ declare global {
   }
 
   interface __PlayerChildGd__Trees {
-    "res://Player.tscn": _PlayerTscn_Tree['Sprite2D/AnimationPlayer']
+    "res://Player.tscn": _PlayerTscn_AnimationPlayer
   }
 
   // script class instances for tree [__node_type] resolution
@@ -75,7 +87,7 @@ declare global {
     "res://Player.tscn": _PlayerTscn_Tree
   }
   interface GodotResources {
-    "res://Player.tscn": PackedScene<TreeNode<_PlayerTscn_Tree>>;
+    "res://Player.tscn": PackedScene<TreeNode<_GDTreeGetType<_PlayerTscn_Tree>, _PlayerTscn_Tree>>;
   }
 }
 
